78f2611ad169e93c1a33b4d1cf6fbe8de24a8cf6
Radoq <rpamula92@gmail.com>
2018-02-12 19:35:25 +0100

    es6 changes
    update to es6.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#search").click(function() {
-        var searchTerm = $("#searchTerm").val();
-        var url = "https://pl.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+        const searchTerm = $("#searchTerm").val();
+        const url = "https://pl.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
 
         $.ajax({
             type: "GET",
@@ -10,7 +10,7 @@ $(document).ready(function() {
             dataType: "json",
             success: function(data) {
                 $("#output").html("");
-                for (var i = 0; i < data[1].length; i++) {
+                for (let i = 0; i < data[1].length; i++) {
                     $('#output').append("<li><a href=" + data[3][i] + " target='_blank'>" + data[1][i] + "</a><p>" + data[2][i] + "</p></li>");
                 }
             },
